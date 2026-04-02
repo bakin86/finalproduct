5dd709304cfb6a7b6f51d3db7361436c10ec92f3
bakin86 <clouden948@gmail.com>
2026-04-02 20:30:41 +0800

    Update prisma.js5

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,16 +2,6 @@ import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 dotenv.config();
 
-const globalForPrisma = globalThis;
-
-const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient();
 
 export default prisma;
