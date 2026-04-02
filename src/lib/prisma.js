1f2c39ddbe231ad814d8fe0495b1e93912a77933
bakin86 <clouden948@gmail.com>
2026-04-02 20:19:57 +0800

    Update prisma.js

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,17 +1,7 @@
-import { PrismaClient } from "../generated/prisma/client.ts";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 dotenv.config();
 
-const adapter = new PrismaMariaDb({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "gank",
-  database: "chatapp",
-  connectionLimit: 5,
-});
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
